37671e951564e8c2b242eefbc0a95df4306efcb6
MarcoAntonio525 <84193439+MarcoAntonio525@users.noreply.github.com>
2026-05-15 20:13:03 -0600

    Update mul.cpp

--- vm/mul.cpp
+++ vm/mul.cpp
@@ -1,7 +1,7 @@
 #include "mul.hpp"
 
-MUL::MUL() : Instruccion("MUL", 3, 2) {}
+MUL::MUL() : Instruccion("MUL", 82, 2) {}
 
 int MUL::execute(ALU& alu, int op1, int op2) {
-    return alu.add(op1, op2);
+    return alu.mul(op1, op2);
 }
