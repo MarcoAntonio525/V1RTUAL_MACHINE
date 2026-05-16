f73221642a1e66c945e03a9a093220c70e0ddcc2
MarcoAntonio525 <84193439+MarcoAntonio525@users.noreply.github.com>
2026-05-15 20:07:40 -0600

    Refactor Program class to use vectors for instructions

--- vm/program.cpp
+++ vm/program.cpp
@@ -1,36 +1,25 @@
 #include "Program.hpp"
 
-Program::Program() : size(0) {
-    // Inicializar operandos en 0
-    for (int i = 0; i < MAX_INSTRUCTIONS; i++) {
-        operand1[i] = 0;
-        operand2[i] = 0;
-    }
+Program::Program() {}
+
+void Program::addInstruction(Instruccion inst, int op1, int op2) {
+    instrucciones.push_back(inst);
+    operandos1.push_back(op1);
+    operandos2.push_back(op2);
 }
 
-bool Program::addInstruction(Instruccion theInstruction, int op1, int op2) {
-    if (size >= MAX_INSTRUCTIONS) {
-        return false;  // programa lleno
-    }
-    instructions[size] = theInstruction;
-    operand1[size] = op1;
-    operand2[size] = op2;
-    size++;
-    return true;
+Instruccion Program::getInstruction(int index) const {
+    return instrucciones[index];
 }
 
-Instruccion Program::getInstruction(int position) const{
-    return instructions[position];
+int Program::getOperand1(int index) const {
+    return operandos1[index];
 }
 
-int Program::getOperand1(int position) const{
-    return operand1[position];
+int Program::getOperand2(int index) const {
+    return operandos2[index];
 }
 
-int Program::getOperand2(int position) const{
-    return operand2[position];
-}
-
-int Program::getSize() const{
-    return size;
+int Program::getSize() const {
+    return instrucciones.size();
 }
