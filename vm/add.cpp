5a071c0dcbbc494708255ec5fe0f993acc757f9b
MarcoAntonio525 <84193439+MarcoAntonio525@users.noreply.github.com>
2026-05-15 20:11:36 -0600

    Update add.cpp

--- vm/add.cpp
+++ vm/add.cpp
@@ -1,7 +1,6 @@
-// ADD.cpp
 #include "ADD.hpp"
 
-ADD::ADD() : Instruccion("ADD", 1, 2) {}  // siempre igual
+ADD::ADD() : Instruccion("ADD", 80, 2) {}
 
 int ADD::execute(ALU& alu, int op1, int op2) {
     return alu.add(op1, op2);
