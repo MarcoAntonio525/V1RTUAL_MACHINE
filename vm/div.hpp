be770dcb1d06ccb6a3e8871867b8b37216de7fd6
MarcoAntonio525 <84193439+MarcoAntonio525@users.noreply.github.com>
2026-05-15 20:13:25 -0600

    Update div.hpp

--- vm/div.hpp
+++ vm/div.hpp
@@ -1,13 +1,13 @@
-// ADD.hpp
-#ifndef ADD_HPP
-#define ADD_HPP
+#ifndef DIV_HPP
+#define DIV_HPP
+
 #include "Instruccion.hpp"
 #include "ALU.hpp"
 
-// ADD.hpp - Suma
 class DIV : public Instruccion {
 public:
-    DIV() : Instruccion("DIV", 4, 2) {}
-    int execute(ALU& alu, int op1, int op2) { return alu.div(op1, op2); }
+    DIV();
+    int execute(ALU& alu, int op1, int op2) override;
 };
+
 #endif
