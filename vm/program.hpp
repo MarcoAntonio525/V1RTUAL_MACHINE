6244bbc6199820037a68a3717f00b6b59d697938
MarcoAntonio525 <84193439+MarcoAntonio525@users.noreply.github.com>
2026-05-15 19:48:06 -0600

    Refactor Program class to use vectors for instructions

--- vm/program.hpp
+++ vm/program.hpp
@@ -1,24 +1,22 @@
 #ifndef PROGRAM_HPP
 #define PROGRAM_HPP
 
+#include <vector>
 #include "Instruccion.hpp"
-#include <string>
 
 class Program {
 private:
-    static const int MAX_INSTRUCTIONS = 10;
-    Instruccion instructions[MAX_INSTRUCTIONS];  // instrucciones
-    int operand1[MAX_INSTRUCTIONS];              // primer operando
-    int operand2[MAX_INSTRUCTIONS];              // segundo operando
-    int size;                                     // cantidad real
-    
+    std::vector<Instruccion> instrucciones;
+    std::vector<int> operandos1;
+    std::vector<int> operandos2;
+
 public:
     Program();
-    bool addInstruction(Instruccion theInstruction, int op1, int op2);
-    Instruccion getInstruction(int position) const;  
+    void addInstruction(Instruccion inst, int op1, int op2);
+    Instruccion getInstruction(int index) const;
+    int getOperand1(int index) const;
+    int getOperand2(int index) const;
     int getSize() const;
-    int getOperand1(int position) const;
-    int getOperand2(int position) const;
 };
 
 #endif
