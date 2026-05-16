e001b4946a1ddd7aa0d511495f2db85244a1ceff
MarcoAntonio525 <84193439+MarcoAntonio525@users.noreply.github.com>
2026-05-15 20:13:40 -0600

    Update div.cpp

--- vm/div.cpp
+++ vm/div.cpp
@@ -1,8 +1,7 @@
-// ADD.cpp
 #include "div.hpp"
 
-DIV::DIV() : Instruccion("DIV", 4, 2) {}
+DIV::DIV() : Instruccion("DIV", 83, 2) {}
 
 int DIV::execute(ALU& alu, int op1, int op2) {
-    return alu.add(op1, op2);
+    return alu.div(op1, op2);
 }
